5dc23af7174f1a69228cfa59feede7c6a961cf86
WardDepoorter <W.Depoorter@student.tudelft.nl>
2025-12-14 17:31:59 +0100

    added grail gravity anomaly mapping

--- img2mx.m
+++ img2mx.m
@@ -1,11 +1,11 @@
 function [TopoMx] = img2mx(filename,px)
 fid = fopen(filename, 'r', 'ieee-le');
-img = fread(fid, [360*px, 180*px], 'int16'); 
+img = fread(fid, [360*px, 180*px+1], 'int16'); 
 fclose(fid);
 
 img = img';
-height = double(img) * 0.5;   % scaling factor(see https://pds-geosciences.wustl.edu/lro/lro-l-lola-3-rdr-v1/lrolol_1xxx/DATA/LOLA_GDR/CYLINDRICAL/IMG/LDEM_4.LBL)
-TopoMx = height;
+%height = double(img) * 0.5;   % scaling factor(see https://pds-geosciences.wustl.edu/lro/lro-l-lola-3-rdr-v1/lrolol_1xxx/DATA/LOLA_GDR/CYLINDRICAL/IMG/LDEM_4.LBL)
+TopoMx = img;
 
 end
 
--- plot_topography.m
+++ plot_topography.m
@@ -1,7 +1,7 @@
 % Parameters
 px = 4; % pixels per degree
 filename = ['Topography/LDEM_4.IMG'];
-Mx = img2mx(filename, px);
+Mx = img2mx(filename, px)/1000;
 Mx = [Mx(:, 181*px:end), Mx(:, 1:180*px)]; % Shift the center of the map 180 degrees
 
 % Plot
@@ -11,6 +11,6 @@ set(gca, 'YDir', 'normal');  % so latitude increases upward
 demcmap(Mx); % Using a better colormap for topography visualization
 colorbar();
 
-title('Lunar topography in km from reference speriod');
+%title('Lunar topography in km from reference speriod');
 xlabel('Longitude (deg)');
 ylabel('Latitude (deg)');
