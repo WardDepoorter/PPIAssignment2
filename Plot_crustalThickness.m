2a080083da7182c09b92c3fead82ce9604ccd57f
WardDepoorter <W.Depoorter@student.tudelft.nl>
2025-12-14 17:38:46 +0100

    upload plots

--- Plot_crustalThickness.m
+++ Plot_crustalThickness.m
@@ -7,7 +7,7 @@ Mx = txt2mx(filename);
 figure;
 imagesc([-180 180], [-90 90], Mx);
 set(gca, 'YDir', 'normal');  % so latitude increases upward
-demcmap(Mx, 128); % Using a better colormap for topography visualization
+%demcmap(Mx, 128); % Using a better colormap for topography visualization
 colorbar();
 
 %title('Crustal Thickness');
